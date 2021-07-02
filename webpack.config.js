814821bb2406ee32e23b70722cc0379665e7c5d8
v.kolesnyk <v.kolesnyk@autodoc.de>
2021-07-02 19:04:54 +0300

    first start

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.[chunkhash].js',
+        path: path.resolve(__dirname, 'publick')
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+    },
+}
